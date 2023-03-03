f20eaff421f83f344626272c8c53aefa305042d1
yngrk <yngrk.kim@gmail.com>
2023-03-03 21:59:53 +0100

    changes for testing in p5

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE_ENV === 'test') {
+  // eslint-disable-next-line global-require
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
